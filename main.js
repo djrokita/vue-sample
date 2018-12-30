e18fab505190c8f486e9d729459a8eab197fffd2
Krzysztof Pierzchała <krzysztof.pierzchala@bluebrick.pl>
2018-12-30 13:46:04 +0100

    Set html and js file with vue instance

---
+++ main.js
@@ -0,0 +1,44 @@
+new Vue({
+    el: '#app',
+    data: {
+        link: {
+            href: 'https://www.wyborcza.pl',
+            target: '_blank',
+            title: 'Gazeta Wyborcza'
+        },
+        foo: '',
+        dest: 'Wyborcza',
+        hobbys: {
+            forex: {
+                eurusd: 'buy',
+                dax: 'sell',
+                sp30: 'hold'
+            },
+            coding: {
+                language: 'javascript',
+                framework: 'vue.js',
+                site: 'frontend'
+            }
+        },
+        income: 0,
+        tax: 19,
+        colors: {
+            0: 'lightblue',
+            1: 'lightgreen',
+            2: 'yellowgreen'
+        },
+        styleColor: 0
+    },
+    methods: {
+        countSalary() {
+            console.log('countSalary was running');
+            return Math.floor(this.income - (this.income * this.tax / 100));
+        }
+    },
+    computed: {
+        computeSalary() {
+            console.log('countSalary was computed');
+            return Math.floor(this.income - (this.income * this.tax / 100));
+        }
+    }
+});
